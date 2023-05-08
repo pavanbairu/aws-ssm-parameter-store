9ec0c03225167ab9e7574a078bcba71d061af921
pavanbairu <pavan.bairu1@gmail.com>
2023-05-08 23:12:28 +0530

    created key management system

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "dev.test1"
-  type  = "String"
+  type  = "SecureString"
   value = "Hello World!"
+  key_id = "1dfa7b43-08ea-4a7f-b85d-4827823ac62e" #will get this from aws key management system key creation
 }
