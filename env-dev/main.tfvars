b68a5095999380da7df6700b72136d1ffb371302
pavanbairu <pavan.bairu1@gmail.com>
2023-05-08 23:34:02 +0530

    created multiple key management system

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.pavanbairu.tech:8080/"},
+  { name = "dev.frontend.cart_url", value = "http://cart-dev.pavanbairu.tech:8080/"}
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "dev.test1"
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
   type  = "SecureString"
-  value = "Hello World!"
+  value = var.parameters[count.index].value
   key_id = "1dfa7b43-08ea-4a7f-b85d-4827823ac62e" #will get this from aws key management system key creation
 }
